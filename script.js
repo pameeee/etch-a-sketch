662fe0624b8c5d6cdbc49f8f7e478ca11410971c
pameeee <65885430+pameeee@users.noreply.github.com>
2023-08-18 14:07:08 +0800

    Test adding div in javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+const numberOfSquares = 16;
+
+var container = document.querySelector(".container");
+
+var newDiv = document.createElement("div");
+newDiv.textContent = "This is a new div";
+container.append(newDiv);
+
+function createBoxes(numberOfSquares) {
+    //
+}
