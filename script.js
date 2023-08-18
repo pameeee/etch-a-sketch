e8dd31e49f016130339f27b01b42afb757d429bb
pameeee <65885430+pameeee@users.noreply.github.com>
2023-08-18 15:53:45 +0800

    [Experiment] Try 4x4 boxes, 25% with

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const numberOfSquares = 16;
-
-var container = document.querySelector(".container");
-
-// var newDiv = document.createElement("div");
-// newDiv.textContent = "This is a new div";
-// container.append(newDiv);
+const numberOfSquares = 4;
 
 function createBoxes(numberOfSquares) {
-    //
+    for (let i = 0; i < 5; i++) {
+        var container = document.querySelector(".container");
+        var newBox = document.createElement("div");
+        newBox.classList.add("box");
+        container.append(newBox);
+    }
 }
+
+createBoxes();
