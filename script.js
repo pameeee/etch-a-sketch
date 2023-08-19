47f38c9f7ed8faa9eb186eeaa3cc48241df76f30
pameeee <65885430+pameeee@users.noreply.github.com>
2023-08-19 13:47:09 +0800

    Fix button functionality

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
-let numberOfSquares = 16;
-const dimension = 100 / numberOfSquares;
+let numberOfSquares = 10;
+let dimension = 100 / numberOfSquares;
+let container = document.querySelector(".container");
 
-function createBoxes() {
-    for (let i = 0; i < numberOfSquares ** 2; i++) {
-        let container = document.querySelector(".container");
+function createBoxes(input) {
+    console.log(`Creating new container with input `, input);
+    container.innerHTML = "";
+    console.log("Just deleted the container");
+    
+    for (let i = 0; i < input ** 2; i++) {
+        
         let newBox = document.createElement("div");
         newBox.classList.add("box");
         newBox.style.width = `${dimension}%`;
         newBox.style.height = `${dimension}%`;
         container.append(newBox);
+        console.log("Created box ", (i + 1));
     }
-}
 
-createBoxes();
-
-const boxes = document.querySelectorAll(".box");
-
-boxes.forEach((box) => 
-    box.addEventListener(
-        "mouseenter", () => {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) =>
+        box.addEventListener("mouseenter", () => {
             box.style.backgroundColor = "green";
-        }
-    )
-);
+        })
+    );
+}
 
 document.querySelector("button").addEventListener(
     "click", function() {
         const userInput = prompt("Pick a number:");
         numberOfSquares = userInput;
+        dimension = 100 / numberOfSquares;
         console.log("New numberOfSquares is ", numberOfSquares);
+        createBoxes(numberOfSquares);
     }
 );
+
+createBoxes(16);
