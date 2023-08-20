a5a831c43b5152f0aff7c251c155c42010f9c945
pameeee <65885430+pameeee@users.noreply.github.com>
2023-08-20 08:57:44 +0800

    Make random button functional

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let container = document.querySelector(".container");
-
-function generateRandomRGBColor() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    return `rgb(${red}, ${green}, ${blue})`;
-}
+let mode = "normal";
 
 function createBoxes(input) {
     const dimension = 100 / input;
+    let color = "green";
     container.innerHTML = "";
+    function generateRandomRGBColor() {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
 
     for (let i = 0; i < input ** 2; i++) {
         let newBox = document.createElement("div");
@@ -22,20 +23,29 @@ function createBoxes(input) {
     const boxes = document.querySelectorAll(".box");
     boxes.forEach((box) =>
         box.addEventListener("mouseenter", () => {
-            box.style.backgroundColor = generateRandomRGBColor();
+            if (mode == "randomColor") {
+                box.style.backgroundColor = generateRandomRGBColor();
+            } else {
+                box.style.backgroundColor = "green";
+            }
         })
     );
 }
 
-document.querySelector("button").addEventListener("click", function () {
+document.getElementById("promptButton").addEventListener("click", function () {
     const userInput = prompt("Pick a number: ");
     if (userInput <= 100) {
         let numberOfSquares = userInput;
         dimension = 100 / numberOfSquares;
+        mode = "normal";
         createBoxes(numberOfSquares);
     } else {
         alert("Please enter a valid number not greater than 100.");
     }
+});
+
+document.getElementById("randomColor").addEventListener("click", function () {
+    mode = "randomColor";
 });
 
 createBoxes(5);
